a2cd8eb76950bbbf979827cd144274a456d29177
Lyahasik <48154267+Lyahasik@users.noreply.github.com>
2022-07-03 22:01:59 +0300

    update items
    Добавлено:
    - текстура помех
    - шейдер волны помех
    - вращение навыков
    
    Обновлено:
    - материал для навыков
    - модели для навыков

--- Assets/Scripts/Game/Level/Items/Item.cs
+++ Assets/Scripts/Game/Level/Items/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using Baraboom.Game.Tools.DiscreteWorld;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -7,11 +8,23 @@ namespace Baraboom.Game.Level.Items
     [RequireComponent(typeof(DiscreteCollider))]
     public sealed class Item : MonoBehaviour
     {
+        [SerializeField] private float _speedRotation;
+        
         private IEffect _effect;
 
         private void Awake()
         {
             _effect = GetComponent<IEffect>();
+        }
+
+        private void Start()
+        {
+            transform.rotation = Quaternion.Euler(-90f, 0f, 0f);
+        }
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _speedRotation * Time.deltaTime);
         }
 
         [UsedImplicitly]
