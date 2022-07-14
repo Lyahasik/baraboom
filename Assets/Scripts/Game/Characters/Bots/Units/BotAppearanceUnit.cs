73fd2c406749c46f3442312119a1ff6504b81876
v.belchenko <belchenkovova@gmail.com>
2022-07-14 23:05:03 +0300

    Bot - change color with animation.

--- Assets/Scripts/Game/Characters/Bots/Units/BotAppearanceUnit.cs
+++ Assets/Scripts/Game/Characters/Bots/Units/BotAppearanceUnit.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using Baraboom.Core.Tools;
+using Baraboom.Core.UI;
 using Baraboom.Game.Characters.Bots.Protocols;
 using UnityEngine;
 
@@ -11,7 +14,10 @@ namespace Baraboom
 		{
 			set
 			{
-				_material.SetColor(_emissionColorId, value ? _colorAggressive : _colorBase);
+				if (this == null)
+					return;
+
+				ChangeColor(value ? _colorAggressive : _colorMain);
 			}
 		}
 
@@ -24,12 +30,35 @@ namespace Baraboom
 		[SerializeField] private Color _colorAggressive;
 
 		private Material _material;
-		private Color _colorBase;
+		private Color _colorMain;
+		private Coroutine _changeColorCoroutine;
 
 		private void Awake()
 		{
 			_material = GetComponentInChildren<MeshRenderer>().material;
-			_colorBase = _material.GetColor(_emissionColorId);
+			_colorMain = _material.GetColor(_emissionColorId);
+		}
+
+		private void ChangeColor(Color targetColor)
+		{
+			if (_changeColorCoroutine != null)
+				StopCoroutine(_changeColorCoroutine);
+
+			_changeColorCoroutine = StartCoroutine(ChangeColorRoutine(_material.GetColor(_emissionColorId), targetColor));
+		}
+
+		private IEnumerator ChangeColorRoutine(Color start, Color finish)
+		{
+			yield return Coroutines.Update(
+				phase =>
+				{
+					if (this != null)
+						_material.SetColor(_emissionColorId, Color.Lerp(start, finish, phase));
+				},
+				UIConstants.ShortenedAnimationDuration
+			);
+
+			_changeColorCoroutine = null;
 		}
 
 		#endregion
